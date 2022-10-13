978e03972861472ff8dcf0bb85315e3b64020c1d
Jeongsu Lee <107098751+JeongSu0880@users.noreply.github.com>
2022-10-13 11:59:22 +0900

    add
    - react bootstrap 설치
    - 사이드 네비게이션 바 구현 (진행)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SideNav from './components/SideNav';
+import Home from './components/Home';
 import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 import { Eventcalendar, getJson, toast, localeKo } from '@mobiscroll/react';
 
 function App() {
+    return(
+    <>
+    <Header />
+    <Home />
+    <SideNav />
+    <Footer />
 
-    const [myEvents, setEvents] = React.useState([]);
+    </>
+    );
 
-    React.useEffect(() => {
-        getJson('https://trial.mobiscroll.com/events/?vers=5', (events) => {
-            setEvents(events);
-        }, 'jsonp');
-    }, []);
+    // const [myEvents, setEvents] = React.useState([]);
+
+    // React.useEffect(() => {
+    //     getJson('https://trial.mobiscroll.com/events/?vers=5', (events) => {
+    //         setEvents(events);
+    //     }, 'jsonp');
+    // }, []); //데이터 set
     
-    const onEventClick = React.useCallback((event) => {
-        toast({
-            message: event.event.title
-        });
-    }, []);
+    // const onEventClick = React.useCallback((event) => {
+    //     toast({
+    //         message: event.event.title
+    //     });
+    // }, []);
     
-    const view = React.useMemo(() => {
-        return {
-            schedule: { type: 'week' }
-        };
-    }, []);
+    // const view = React.useMemo(() => {
+    //     return {
+    //         schedule: { type: 'week' }
+    //     };
+    // }, []);
 
-    return (
-        <Eventcalendar
-            theme="ios" 
-            themeVariant="light"
-            clickToCreate={true}
-            dragToCreate={true}
-            dragToMove={true}
-            dragToResize={true}
-            eventDelete={true}
-            locale={localeKo}
-            data={myEvents}
-            view={view}
-            onEventClick={onEventClick}
-       />
-    ); 
+    // return (
+    //     <Eventcalendar
+    //         theme="ios" 
+    //         themeVariant="light"
+    //         clickToCreate={true}
+    //         dragToCreate={true}
+    //         dragToMove={true}
+    //         dragToResize={true}
+    //         eventDelete={true}
+    //         locale={localeKo}
+    //         data={myEvents}
+    //         view={view}
+    //         onEventClick={onEventClick}
+    //    />
+    // ); 
 }
 
 export default App;
