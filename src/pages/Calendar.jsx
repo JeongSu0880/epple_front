583bead8f7d202751f03920f23d3efd29240c409
Nahyun39 <nhhhh3999@gmail.com>
2022-10-16 03:36:39 +0900

    [ADD]Calendar page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {ScheduleComponent, ViewsDirective, 
+Day, Week, WorkWeek, Month, 
+Agenda, Inject, Resize, DragAndDrop} 
+from '@syncfusion/ej2-react-schedule';
+import {DatePickerComponent} from '@syncfusion/ej2-react-calendars';
+import {ScheduleData} from '../data/dummy';
+import "../styles/Calendar.css";
 
 const Calendar = () => {
     return (
-        <div>
-            <h1>캘린더 페이지</h1>
+        <div className="m-2 md:m-10 mt-24 p-2
+        md:p10 bg-white rounded-3xl">
+            {/* <Header category="App" title="Calendar"/> */}
+            <div className="header">
+                <h2>Calendar</h2>
+            </div>
+            <ScheduleComponent width="1000px" height="960px">
+                <Inject services={[Day, Week, WorkWeek, Month,
+                Agenda, Resize, DragAndDrop]} />
+            </ScheduleComponent>
         </div>
     );
 }
+
 
 export default Calendar;
